7200ca3decdc8775537c6f41fce9815c65c68a3f
rmatharoo <rsmatharoo@outlook.com>
2019-08-18 15:55:31 -0400

    New updates csv generation, openmaps api

--- js/main.js
+++ js/main.js
@@ -1,63 +1,64 @@
-//Author: Rupinder Singh Matharoo 
+//Author: Rupinder Singh Matharoo
 //Dated: January 20,2014
 // This small Application uses jQuery, JSON, and Google GeoCode API to get the Address Coordinates. The query if valid returns number of results with link and coordinates. App also allows you to reverse geocode the longitude and latitude values to give you the address suggestions returned by the Google geocode API.
 $(document).ready(function(){
         //hide the menu when app starts..
         $("#geocoding").hide();
+        $("#exportcoords").hide();
+        $("#exportaddr").hide();
         $("#reverse").hide();
+        var geotype ="";
         //Geocoding the addresses
         $("#getgeo").click(function(){
+            geotype="GeoCoordinates";
             $("#geo").find("tbody").remove();
             var add= $("#add").val();
             if(add==""){
                 $('#geo').append('<tr><td>Please enter something!<br></td></tr>');
             }
-            else{   
-             $.getJSON('http://maps.google.com/maps/api/geocode/json?sensor=false&address='+add, function(jd) {
-                 $("#geo").append('<tr><th>Address</th> <th>Latitude</th><th>Longitude</th></tr>'); 
-                if(jd.status=="OK"){
-                    for(var i=0; i<jd.results.length; i++){
-                    var lat= jd.results[i].geometry.location.lat;
-                    var lng= jd.results[i].geometry.location.lng;
-                    var link = "http://maps.google.com/?q="+lat+","+lng;
-                    $('#geo').append('<tr><td><a target="_blank" href='+link+'>' + jd.results[i].formatted_address +'</a></td>'+ 
-                                     '<td>' + lat + '</td>' + 
-                                     '<td>' + lng +'</td>' + 
+            else{
+             $.getJSON('http://open.mapquestapi.com/geocoding/v1/address?key=Sf1M5KMV6oofg1zYOG0A2AQkWy8ExwkF&location='+add, function(jd) {
+                 $("#geo").append('<tr><th>Address</th> <th>Latitude</th><th>Longitude</th></tr>');
+                if(jd.info.statuscode==0 && jd.results[0].locations.length>0){
+                  $("#exportcoords").show();
+                    for(var i=0; i<jd.results[0].locations.length; i++){
+                    var lat= jd.results[0].locations[i].latLng.lat;
+                    var lng= jd.results[0].locations[i].latLng.lng;
+                    var linkText = "";
+                    if(jd.results[0].locations[i].adminArea6!=undefined){
+                      linkText += jd.results[0].locations[i].adminArea6+" ";
+                    }
+                    if(jd.results[0].locations[i].adminArea5!=undefined){
+                      linkText += jd.results[0].locations[i].adminArea5+" ";
+                    }
+                    if(jd.results[0].locations[i].adminArea4!=undefined){
+                      linkText += jd.results[0].locations[i].adminArea4+" ";
+                    }
+                    if(jd.results[0].locations[i].adminArea3!=undefined){
+                      linkText += jd.results[0].locations[i].adminArea3+" ";
+                    }
+                    if(jd.results[0].locations[i].adminArea2!=undefined){
+                      linkText += jd.results[0].locations[i].adminArea2+" ";
+                    }
+                    if(jd.results[0].locations[i].adminArea1!=undefined){
+                      linkText += jd.results[0].locations[i].adminArea1+" ";
+                    }
+                    $('#geo').append('<tr><td><a target="_blank" href='+jd.results[0].locations[i].mapUrl+'>' + linkText +'</a></td>'+
+                                     '<td><p>' + lat + '</p></td>' +
+                                     '<td>' + lng +'</td>' +
                                      '</tr>');
                      }
                  }
-                 if(jd.status=="ZERO_RESULTS"){
+                 else{
                         $('#geo').append('<tr><td>'+add+' not found! <br></td></tr>');
                          }
               });
             }
         });
-    /*
-    $("#icon").click(function(){
-        console.log("hello icon click!");
-        var Geo={};
 
-        if (navigator.geolocation) {
-           navigator.geolocation.getCurrentPosition(success, error);
-        }
-
-        //Getting your location coords
-        function success(position) {
-            Geo.lat = position.coords.latitude;
-            Geo.lng = position.coords.longitude;
-            console.log("latitude and longitude: "+Geo.lat+Geo.lng);
-            $("#geo").append('<tr><th>My Latitude</th><th>My Longitude</th></tr>');
-            $("#geo").append('<tr><td>'+Geo.lat+'</td><td>'+Geo.lng+'</td></tr>');
-        }
-
-        function error(){
-            console.log("Geocoder failed");
-        }
-        
-    });
-    */
         //Reverse Geocoding
         $("#getadd").click(function(){
+          geotype="ReverseGeoAddresses";
             $("#geo").find("tbody").remove();
             var latlng= $("#lat").val()+","+$("#lng").val();
             //console.log(latlng);
@@ -65,25 +66,39 @@ $(document).ready(function(){
                 $('#geo').append('<tr><td>Please enter both Latitude and Longitude values<br></td></tr>');
             }
             if($("#lat").val()!="" && $("#lng").val()!=""){
-             $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?sensor=false&latlng='+latlng, function(jd) {
-                 $("#geo").append('<tr><th>Approx. Address</th> <th>Country</th><th>ISO Code</th></tr>'); 
-                if(jd.status=="OK"){
+             $.getJSON('http://www.mapquestapi.com/geocoding/v1/reverse?key=Sf1M5KMV6oofg1zYOG0A2AQkWy8ExwkF&location='+$("#lat").val()+','+$("#lng").val()+'&includeRoadMetadata=true&includeNearestIntersection=true', function(jd) {
+                 $("#geo").append('<tr><th>Approx. Address</th> <th>Country</th><th>State/Province</th></tr>');
+                if(jd.info.statuscode==0 && jd.results[0].locations.length>0){
+                    $("#exportaddr").show();
                    // console.log(jd.results.length);
-                    for(var i=0; i<jd.results.length; i++){
-                    var addr= new String;
-                    addr= jd.results[i].formatted_address;
+                    for(var i=0; i<jd.results[0].locations.length; i++){
+                    var addr = "";
+                    if(jd.results[0].locations[i].street!=undefined){
+                      addr += jd.results[0].locations[i].street+" ";
+                    }
+                    if(jd.results[0].locations[i].adminArea6!=undefined){
+                      addr += jd.results[0].locations[i].adminArea6+" ";
+                    }
+                    if(jd.results[0].locations[i].adminArea5!=undefined){
+                      addr += jd.results[0].locations[i].adminArea5+" ";
+                    }
+                    if(jd.results[0].locations[i].adminArea4!=undefined){
+                      addr += jd.results[0].locations[i].adminArea4+" ";
+                    }
+                    if(jd.results[0].locations[i].adminArea3!=undefined){
+                      addr += jd.results[0].locations[i].adminArea3+" ";
+                    }
+                    if(jd.results[0].locations[i].adminArea2!=undefined){
+                      addr += jd.results[0].locations[i].adminArea2+" ";
+                    }
+                    if(jd.results[0].locations[i].adminArea1!=undefined){
+                      addr += jd.results[0].locations[i].adminArea1+" ";
+                    }
                     //encoding the address value to concatenate into the link...
-                    var links = "http://maps.google.com/?q="+encodeURIComponent(addr);
-                    var country = "";
-                    var ctrcode = "";
-                        for(var j=0; j<jd.results[i].address_components.length; j++){
-                            if(jd.results[i].address_components[j].types[0] =="country")
-                            {
-                                country = jd.results[i].address_components[j].long_name;
-                                ctrcode = jd.results[i].address_components[j].short_name;
-                            }   
-                        }
-                    $('#geo').append('<tr><td><a target="_blank" href='+links+'>' + jd.results[i].formatted_address +'</a></td>'+
+                    var links = jd.results[0].locations[i].mapUrl;
+                    var country = jd.results[0].locations[i].adminArea1;
+                    var ctrcode = jd.results[0].locations[i].adminArea3;
+                    $('#geo').append('<tr><td><a target="_blank" href='+links+'>' + addr +'</a></td>'+
                                      '<td>' + country + '</td>' +
                                      '<td>'+ ctrcode + '</td>' +
                                      '</tr>');
@@ -98,40 +113,44 @@ $(document).ready(function(){
         $("#clear1").click(function(){
             $("#geo").find("tbody").remove();
         });
-        
+
         $("#clear2").click(function(){
             $("#geo").find("tbody").remove();
             $("#lat").val('');
             $("#lng").val('');
         });
-        
+
         $("#radgeo").click(function(){
             if($("#radgeo").is(":checked")){
                 $("#footer").hide();
                 $("#geo").find("tbody").remove();
                 $("#geocoding").show(500).fadeIn;
                 $("#reverse").hide(500).fadeIn;
+                $("#exportaddr").hide();
+                $("#exportcoords").hide();
             }
         });
-        
+
         $("#radrev").click(function(){
             if($("#radrev").is(":checked")){
                 $("#footer").hide();
                 $("#geo").find("tbody").remove();
                 $("#geocoding").hide(500).fadeIn;
                 $("#reverse").show(500).fadeIn;
+                $("#exportaddr").hide();
+                $("#exportcoords").hide();
             }
         });
-    
+
         $("#lat,#lng").keydown(function (e) {
             // Allow: backspace, delete, tab, escape, enter and .
             if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190, 189, 45]) !== -1 ||
                  // Allow: Ctrl+A
-                (e.keyCode == 65 && e.ctrlKey === true) || 
+                (e.keyCode == 65 && e.ctrlKey === true) ||
                  // Allow: home, end, left, right
                 (e.keyCode >= 35 && e.keyCode <= 39)) {
                      // let it happen, don't do anything
-                console.log("pressed",e.keyCode);
+                //console.log("pressed",e.keyCode);
                      return;
             }
             // Ensure that it is a number and stop the keypress
@@ -144,5 +163,72 @@ $(document).ready(function(){
                 $("#getgeo").click();
             }
         });
-        
+
+    $('#exportcoords, #exportaddr').click(function() {
+          var titles = [];
+          var data = [];
+          //getting table headers
+          $('.dataTable th').each(function() {
+            titles.push($(this).text());
+          });
+          //getting table data
+          $('.dataTable td').each(function() {
+            data.push($(this).text());
+          });
+          //csv conversion
+          var CSVString = prepCSVRow(titles, titles.length, '');
+          CSVString = prepCSVRow(data, titles.length, CSVString);
+          //downloading csv
+          var downloadLink = document.createElement("a");
+          var blob = new Blob(["\ufeff", CSVString]);
+          var url = URL.createObjectURL(blob);
+          downloadLink.href = url;
+          downloadLink.download = "LatLong-"+geotype+"-"+getFormattedTime()+".csv";
+
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+          document.body.removeChild(downloadLink);
+        });
+
+
+            function prepCSVRow(arr, columnCount, initial) {
+              var row = '';
+              var delimeter = ','; // data slice separator, in excel it's `;`, in usual CSv it's `,`
+              var newLine = '\r\n'; // newline separator for CSV row
+
+              function splitArray(_arr, _count) {
+                var splitted = [];
+                var result = [];
+                _arr.forEach(function(item, idx) {
+                  if ((idx + 1) % _count === 0) {
+                    splitted.push(item);
+                    result.push(splitted);
+                    splitted = [];
+                  } else {
+                    splitted.push(item);
+                  }
+                });
+                return result;
+              }
+              var plainArr = splitArray(arr, columnCount);
+              plainArr.forEach(function(arrItem) {
+                arrItem.forEach(function(item, idx) {
+                  row += item + ((idx + 1) === arrItem.length ? '' : delimeter);
+                });
+                row += newLine;
+              });
+              return initial + row;
+            }
+
+            function getFormattedTime() {
+              var today = new Date();
+              var y = today.getFullYear();
+              // JavaScript months are 0-based.
+              var m = today.getMonth() + 1;
+              var d = today.getDate();
+              var h = today.getHours();
+              var mi = today.getMinutes();
+              var s = today.getSeconds();
+              return y + "-" + m + "-" + d + "-" + h + "-" + mi + "-" + s;
+          }
     });//End of main document ready function
